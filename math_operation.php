5835aca98e062abb11e73f667af6f792112c73db
Getabalewkemaw <getabalewkemaw@gmail.com>
2026-03-19 15:30:41 +0300

    MathOperation using Namespace

---
+++ math_operation.php
@@ -0,0 +1,11 @@
+<?php
+namespace MathOperation;
+class Arthimetic{
+    public function sqrtNum($num)  {
+        return $num *$num;
+        
+    }
+}
+
+
+?>
---
+++ operation.php
@@ -0,0 +1,9 @@
+<?php
+require("math_operation.php");
+use MathOperation\Arthimetic as Ps;
+$res=new Ps();
+echo $res->sqrtNum(10);
+
+
+
+?>
